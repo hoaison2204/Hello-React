65583876d486a2c622479da79c6e8654667c407b
Hoai Son <hoaison2204@gmail.com>
2022-04-01 15:39:42 +0700

    #38

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,6 @@ import { ConnectedRouter as Router } from "connected-react-router";
 import { history } from "../redux";
 import { ToastContainer } from "react-toastify";
 
-// import { AppComponent } from "";
 import {
   userIsAuthenticated,
   userIsNotAuthenticated,
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,5 +2,8 @@ import axios from "../axios";
 const handleLoginApi = (email, password) => {
   return axios.post("/api/login", { email, password });
 };
-
-export { handleLoginApi };
+const getAllUsers = (inputId) => {
+  // template string
+  return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+export { handleLoginApi, getAllUsers };
