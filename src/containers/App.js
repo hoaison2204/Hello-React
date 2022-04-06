00ea9a7d75187860ff9cabc2645cbab656431b30
Hoai Son <hoaison2204@gmail.com>
2022-04-06 23:05:39 +0700

    #48

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,6 +13,7 @@ import Home from "../routes/Home";
 import Login from "./Auth/Login";
 import Header from "./Header/Header";
 import System from "../routes/System";
+import CustomScrollbars from "../components/CustomScrollbars";
 
 import { CustomToastCloseButton } from "../components/CustomToast";
 // import ConfirmModal from "../components/ConfirmModal";
@@ -43,14 +44,16 @@ class App extends Component {
           <div className="main-container">
             {this.props.isLoggedIn && <Header />}
 
-            <span className="content-container">
-              <Switch>
-                <Route path={path.HOME} exact component={Home} />
-                <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
-                <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
-                <Route path={path.HOMEPAGE} component={homePage} />
-              </Switch>
-            </span>
+            <div className="content-container">
+              <CustomScrollbars style={{ height: "100vh", width: "100%" }}>
+                <Switch>
+                  <Route path={path.HOME} exact component={Home} />
+                  <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
+                  <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                  <Route path={path.HOMEPAGE} component={homePage} />
+                </Switch>
+              </CustomScrollbars>
+            </div>
 
             <ToastContainer
               className="toast-container"
