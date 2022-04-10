a575b966d7c4d436adce9245e8b51297eaf82203
Hoai Son <hoaison2204@gmail.com>
2022-04-10 10:53:25 +0700

    #53

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,17 +6,14 @@ import { history } from "../redux";
 import { ToastContainer } from "react-toastify";
 import { userIsAuthenticated, userIsNotAuthenticated } from "../hoc/authentication";
 import { path } from "../utils";
+import { CustomToastCloseButton } from "../components/CustomToast";
 
 import homePage from "./homePage/HomePage.js";
 import Home from "../routes/Home";
-// import Login from '../routes/Login';
 import Login from "./Auth/Login";
-import Header from "./Header/Header";
 import System from "../routes/System";
 import CustomScrollbars from "../components/CustomScrollbars";
 
-import { CustomToastCloseButton } from "../components/CustomToast";
-// import ConfirmModal from "../components/ConfirmModal";
 
 class App extends Component {
   handlePersistorState = () => {
@@ -42,8 +39,6 @@ class App extends Component {
       <Fragment>
         <Router history={history}>
           <div className="main-container">
-            {this.props.isLoggedIn && <Header />}
-
             <div className="content-container">
               <CustomScrollbars style={{ height: "100vh", width: "100%" }}>
                 <Switch>
